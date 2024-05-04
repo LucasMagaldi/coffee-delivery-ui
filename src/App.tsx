9950d45ca514a4dda07a0c6f545d7b28053145fa
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-05-04 20:26:20 -0300

    configure global style and theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/deafault";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       App
-    </>
+    </ThemeProvider>
   )
 }
 
