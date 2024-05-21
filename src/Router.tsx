3db204501affe6f52ad52b1ae6adfc79f2fe8a76
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-05-21 20:27:03 -0300

    add images and data json

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router';
 import { DefaultLayout } from './layouts/DefaultLayout';
 import { CheckOut } from './pages/CheckOut';
+import { Home } from './pages/Home';
 
 export function Router() {
 	return (
 		<Routes>
 			<Route path="/" element={<DefaultLayout />}>
+				<Route path='/' element={<Home />} />
 				<Route path="/check-out" element={<CheckOut />} />
 			</Route>
 		</Routes>
